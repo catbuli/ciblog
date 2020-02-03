c93730c988e93fdceccb60f91c4aef7b3945b943
zhangyifei233 <15204944127@163.com>
2020-02-03 23:52:53 +0800

    feat login页面结构

--- blog/src/views/Login.vue
+++ blog/src/views/Login.vue
@@ -1,11 +1,23 @@
 <template>
     <div id="login">
         <div class="login-bg"></div>
-        <div class="login-main">
+        <div class="login-main"
+             ref="login">
             <div class="login-top">
-                sss
+                <input type="text"
+                       placeholder="name"
+                       v-model="username"
+                       name="username">
+                <input type="password"
+                       placeholder="password"
+                       name="password"
+                       v-model="password"
+                       @keydown.enter="login">
             </div>
-            <div class="login-botton">sss</div>
+            <div class="login-botton"
+                 @click="login">
+                <h3>Login</h3>
+            </div>
         </div>
     </div>
 </template>
@@ -15,19 +27,34 @@ export default {
     name: "login",
     components: {},
     data() {
-        return {};
+        return {
+            username: "",
+            password: ""
+        };
     },
-    methods: {},
+    methods: {
+        login() {
+            var login = this.$refs.login;
+            login.addEventListener("animationend", function() {
+                login.style = "";
+                console.log("删除style");
+            });
+            login.style.animation = "error 0.8s ease 0s 1";
+        }
+    },
     mounted() {}
 };
 </script>
 
-<style scoped>
+<style>
 #login {
     /* display: flex; */
     height: 100%;
     width: 100%;
     position: fixed;
+    display: flex;
+    justify-content: center; /* 水平居中 */
+    align-items: center;
 }
 .login-bg {
     z-index: -10;
@@ -43,22 +70,59 @@ export default {
     filter: blur(20px);
 }
 .login-main {
-    z-index: 1;
-    height: 20px;
-    width: 48%;
-    position: relative;
-    margin: 0 auto;
-    top: 50%;
-    transform: translateY(-50%);
+    /* animation: error 0.8s ease 0s 1; */
 }
 .login-top {
-    width: 100%;
     background: #e1e1e1;
+    padding: 40px 60px;
     border-radius: 25px 25px 0px 0px;
 }
 .login-botton {
-    width: 100%;
-    background: #e1e1e1;
+    padding: 20px 60px;
+    background: #e15748;
     border-radius: 0px 0px 25px 25px;
+    border-top: 2px solid #ad4337;
+    cursor: pointer;
+}
+.login-button:hover {
+}
+@keyframes error {
+    0% {
+        transform: translateX(0px);
+    }
+    25% {
+        transform: translateX(5px);
+    }
+    50% {
+        transform: translateX(-5px);
+    }
+    75% {
+        transform: translateX(5px);
+    }
+    100% {
+        transform: translateX(0px);
+    }
+}
+.login-botton h3 {
+    text-align: right;
+    color: #ffffff;
+    font-size: 35px;
+    margin: 10px auto;
+    user-select: none;
+}
+
+.login-top input {
+    text-align: center;
+    display: block;
+    outline: none;
+    font-size: 25px;
+    font-weight: 500;
+    color: #000000;
+    padding: 15px 20px;
+    background: #cacaca;
+    border: 1px solid #ccc;
+    border-radius: 25px;
+    margin: 0px 0px 12px 0px;
+    /* width: 60%; */
 }
 </style>
