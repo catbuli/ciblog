e99f5c78c2e64821e720f128950cb33e2189dbc1
zhangyifei233 <15204944127@163.com>
2020-02-09 00:45:29 +0800

    update 修改评论状态逻辑

--- blog/src/components/admin/manageComment.vue
+++ blog/src/components/admin/manageComment.vue
@@ -1,15 +1,22 @@
 <template>
     <adminFrame title="评论管理">
-        <el-button type="primary"
-                   icon="el-icon-delete"
-                   @click="delComment">
-        </el-button>
-        <el-radio-group v-model="radio1">
-            <el-radio-button label="是"></el-radio-button>
-            <el-radio-button label="北京"></el-radio-button>
-            <el-radio-button label="广州"></el-radio-button>
-            <el-radio-button label="深圳"></el-radio-button>
-        </el-radio-group>
+        <el-row>
+            <el-col :span=2>
+                <el-button type="primary"
+                           icon="el-icon-delete"
+                           @click="delComment">
+                </el-button>
+            </el-col>
+            <el-col :span=12>
+                <el-radio-group v-model="paging.type"
+                                @change="selectChange">
+                    <el-radio-button label=-1>全部</el-radio-button>
+                    <el-radio-button label=1>通过</el-radio-button>
+                    <el-radio-button label=0>待审核</el-radio-button>
+                    <el-radio-button label=2>垃圾</el-radio-button>
+                </el-radio-group>
+            </el-col>
+        </el-row>
         <el-table class="comment-table"
                   :highlight-current-row="true"
                   :data="commentList"
@@ -28,38 +35,19 @@
                 </template>
             </el-table-column>
             <el-table-column label="内容"
-                             :filters="filters"
-                             :filter-method="filterHandler"
-                             :filter-multiple=false
-                             filter-placement="bottom-start"
                              prop="status">
                 <template slot-scope="scope">
                     <p class="content-table-top">{{scope.row.create_date}}　评论于　<a :href="'/article/'+scope.row.aid">{{scope.row.title}}</a> </p>
                     <p class="content-table-middle"
                        v-text="scope.row.content"></p>
-                    <p class="content-table-bottom"
-                       v-if="scope.row.status===0">
-                        <span @click="editCommentStatus(scope.row.cid,1)">通过</span>
-                        <span class="select">待审核</span>
-                        <span @click="editCommentStatus(scope.row.cid,2)">垃圾</span>
-                        <span @click="editComment(scope.row.cid)">编辑</span>
-                        <span>回复</span>
-                        <span @click="delComment2(scope.row.cid)">删除</span>
-                    </p>
-                    <p class="content-table-bottom"
-                       v-else-if="scope.row.status===1">
-                        <span class="select">通过</span>
-                        <span @click="editCommentStatus(scope.row.cid,0)">待审核</span>
-                        <span @click="editCommentStatus(scope.row.cid,2)">垃圾</span>
-                        <span @click="editComment(scope.row.cid)">编辑</span>
-                        <span>回复</span>
-                        <span @click="delComment2(scope.row.cid)">删除</span>
-                    </p>
-                    <p class="content-table-bottom"
-                       v-else>
-                        <span @click="editCommentStatus(scope.row.cid,1)">通过</span>
-                        <span @click="editCommentStatus(scope.row.cid,0)">待审核</span>
-                        <span class="select">垃圾</span>
+                    <p class="content-table-bottom">
+                        <el-radio-group v-model="scope.row.status"
+                                        size="mini"
+                                        @change="(label)=>{editCommentStatus(label,scope.row.cid)}">
+                            <el-radio-button label=1>通过</el-radio-button>
+                            <el-radio-button label=0>待审核</el-radio-button>
+                            <el-radio-button label=2>垃圾</el-radio-button>
+                        </el-radio-group>
                         <span @click="editComment(scope.row.cid)">编辑</span>
                         <span>回复</span>
                         <span @click="delComment2(scope.row.cid)">删除</span>
@@ -100,6 +88,7 @@ export default {
                 pageSize: 10,
                 currentPage: 1,
                 type: -1,
+                typeName: "status",
                 total: 0
             }
         };
@@ -118,6 +107,11 @@ export default {
         this.getCommentList();
     },
     methods: {
+        selectChange(value) {
+            this.paging.currentPage = 1;
+            this.paging.type = value;
+            this.$store.dispatch("getCommentListAction", this.paging);
+        },
         getCommentList() {
             this.loading = true;
             this.$store.dispatch("getCommentListAction", this.paging);
@@ -151,18 +145,15 @@ export default {
             }
             this.selectRows = flag;
         },
-        editCommentStatus(cid, status) {
+        editCommentStatus(label, cid) {
+            console.log(label, cid);
             this.$store.dispatch("editCommentStatusAction", {
                 cid: cid,
-                status: status
+                status: label
             });
         },
         editComment(cid) {
             // this.isEdit = true;
-        },
-        filterHandler(value, row, column) {
-            console.log(value, row, column);
-            return row.status === value;
         },
         currentChange(e) {
             this.paging.currentPage = e;
@@ -186,21 +177,13 @@ export default {
     margin-right: 10px;
     cursor: pointer;
 }
-.content-table-bottom
-    span:nth-of-type(1)
-    .content-table-bottom
-    span:nth-of-type(2)
-    .content-table-bottom
-    span:nth-of-type(3) {
-    color: rgb(59, 59, 59);
-}
-.content-table-bottom span:nth-of-type(4) {
+.content-table-bottom span:nth-of-type(1) {
     color: green;
 }
-.content-table-bottom span:nth-of-type(5) {
+.content-table-bottom span:nth-of-type(2) {
     color: black;
 }
-.content-table-bottom span:nth-of-type(6) {
+.content-table-bottom span:nth-of-type(3) {
     color: red;
 }
 .select {
