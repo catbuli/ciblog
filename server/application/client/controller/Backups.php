039e80544a90f6489bdd16cad500556977df758f
zhangyifei233 <15204944127@163.com>
2020-02-10 20:37:07 +0800

    feat 备份接口

---
+++ server/application/client/controller/Backups.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\client\controller;
+
+use think\Controller;
+use think\Exception;
+use app\common\Response;
+use think\Db;
+
+class Backups extends Controller
+{
+    /**
+     * 控制器默认方法 获取文章列表
+     *
+     * @return json
+     */
+    public function index()
+    {
+        try {
+            $cmd = 'sudo pwd';
+            exec($cmd, $result, $var);
+            var_dump($result);
+            var_dump($var);
+            // return Response::result(201, "成功", "数据获取成功!", $a);
+        } catch (Exception $e) {
+            return Response::result(400, "请求失败!", $e->getMessage());
+        }
+    }
+}
