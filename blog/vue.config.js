2b40de638c63493e2b7db765cf6c6ca8d8f17769
zhangyifei233 <15204944127@163.com>
2020-02-19 23:49:38 +0800

    update 路由模式改为hash

--- blog/vue.config.js
+++ blog/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     // 博客↑
     devServer: {
         open: true, //是否自动弹出浏览器页面
