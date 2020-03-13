5393bb4e55d4e0572b5d3c7221aafb668a872a5c
zhangyifei233 <15204944127@163.com>
2020-03-13 23:39:33 +0800

    update toolbox 拖拽bug

--- blog/src/components/miniTools/toolbox.vue
+++ blog/src/components/miniTools/toolbox.vue
@@ -2,11 +2,17 @@
     <div id="toolbox"
          ref="toolbox"
          :class="[isShow?'':'hidden']"
-         @mousedown="mousedown"
          @mouseup="mouseup">
+        <div class="drag-bar"
+             @mousedown="mousedown"><i class="el-icon-rank"></i></div>
         <router-link to="/">返回首页</router-link>
-        <a @click="$router.back()">返回</a>
-        <a @click="$router.back(1)">前进</a>
+        <p @click="$router.back()">返回</p>
+        <p @click="$router.back(1)">前进</p>
+        <ul class="category-list">
+            <li v-for="category in categoryList"
+                @click="categoryJump(category.name,category.mid)"
+                :key="category.mid">{{category.name}}</li>
+        </ul>
     </div>
 </template>
 
@@ -28,16 +34,34 @@ export default {
             return this.$store.state.global.headerDOM
                 ? this.$store.state.global.headerDOM
                 : 800;
+        },
+        categoryList() {
+            return this.$store.state.category.categoryList;
         }
     },
     methods: {
+        categoryJump(name, mid) {
+            this.$router.push({
+                path: "/search",
+                query: {
+                    typeName: "category",
+                    type: name,
+                    mid: mid
+                }
+            });
+        },
+        getCategoryList() {
+            this.$store.dispatch("getCategoryListAction");
+        },
         mousedown(e) {
+            console.log(e.currentTarget === e.target);
+            console.log(e);
             this.DOM = this.$refs.toolbox;
             this.body = document.body;
             this.isDrop = true;
-            this.offsetX = e.offsetX;
-            this.offsetY = e.offsetY;
-            document.addEventListener("mousemove", this.mousemove, true);
+            this.offsetX = e.layerX;
+            this.offsetY = e.layerY;
+            document.addEventListener("mousemove", this.mousemove);
         },
         mousemove(e) {
             // console.log(body.offsetWidth);
@@ -78,6 +102,7 @@ export default {
         }
     },
     mounted() {
+        this.getCategoryList();
         window.addEventListener("scroll", this.handleScroll, true);
     },
     destroyed() {
@@ -95,10 +120,31 @@ export default {
     box-shadow: 3px 3px 3px rgb(138, 138, 138);
     width: 100px;
     height: 150px;
-    display: inline-block;
+    display: block;
     position: fixed;
     left: 300px;
     bottom: 150px;
+    .drag-bar {
+        opacity: 1;
+        width: 100%;
+        height: 30px;
+        // position: absolute;
+        top: -30px;
+        left: 0;
+        text-align: center;
+        background: #fff;
+        box-shadow: 0 0 5px #ddd;
+        cursor: move;
+        color: #999;
+        line-height: 30px;
+        font-size: 20px;
+        transition: opacity 0.3s;
+        z-index: 70;
+    }
+    .category-list {
+        color: white;
+        cursor: pointer;
+    }
 }
 .hidden {
     display: none !important;
