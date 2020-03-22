17ea7139dbc3ce6a192c39dbe5abf17db778aa74
zhangyifei233 <15204944127@163.com>
2020-03-22 22:10:46 +0800

    fix 写文章页面双重标题

--- blog/src/pages/admin/components/common/adminFrame.vue
+++ blog/src/pages/admin/components/common/adminFrame.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="admin-frame"
          :style="{ height: height,width:width}">
-        <adminTitle :title="title">
+        <adminTitle :title="title"
+                    v-if="action">
             <i class="el-icon-refresh refresh"
-               v-if="action"
                @click="refresh()"></i>
         </adminTitle>
         <slot></slot>
