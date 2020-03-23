46dccb9bbff4881f418b2dc3a48b0c6378038db8
zhangyifei233 <15204944127@163.com>
2020-03-23 21:13:20 +0800

    fix 文章管理界面分类筛选后定位按钮消失

--- api/application/index/model/Article.php
+++ api/application/index/model/Article.php
@@ -40,7 +40,7 @@ class Article extends Model
                     ->limit($pagingDate, $paging['pageSize'])
                     ->where('title', 'like', '%' . $paging['type'] . '%')
                     ->whereOr('text', 'like', '%' . $paging['type'] . '%')
-                    ->field('aid,title,create_date,pv,comment_count,cover_url,description')
+                    ->field('aid,title,create_date,pv,comment_count,cover_url,description,status')
                     ->select();
             case 'category':
                 $list = Db::name('article_meta')
@@ -52,7 +52,7 @@ class Article extends Model
                 foreach ($list as $value) {
                     array_unshift($dataList, Db::name("article")
                         ->where('aid', $value['aid'])
-                        ->field('aid,title,create_date,pv,comment_count,cover_url,description')
+                        ->field('aid,title,create_date,pv,comment_count,cover_url,description,status')
                         ->find());
                 }
                 return $dataList;
