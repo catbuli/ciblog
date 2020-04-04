8aec9ec6006293502f894a7d60e0166fcad785ef
zhangyifei233 <15204944127@163.com>
2020-04-04 15:15:10 +0800

    refactor 构建配置修改

--- blog/src/pages/index/router.js
+++ blog/src/pages/index/router.js
@@ -1,59 +1,58 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import {
-    post
-} from '@/lib/http';
+import { post } from '@/lib/http';
 
 Vue.use(VueRouter);
 
-const routes = [{
-        path: '/',
-        name: 'home',
-        component: (resolve) => require(['./views/Home.vue'], resolve),
-    },
-    {
-        path: '/search/:typeName/:type',
-        name: 'search',
-        component: (resolve) => require(['./views/Search.vue'], resolve),
-        meta: {
-            title: '搜索'
-        }
-    },
-    {
-        path: '/article/:id',
-        name: 'article',
-        component: (resolve) => require(['./views/Article.vue'], resolve)
-    },
-    {
-        path: '/log',
-        name: 'log',
-        component: (resolve) => require(['./views/Log.vue'], resolve),
-        meta: {
-            title: '归档'
-        }
-    },
-    {
-        path: '*',
-        name: '404',
-        component: (resolve) => require(['./views/404.vue'], resolve),
-        meta: {
-            title: '页面逃走了'
-        }
-    }
+const routes = [
+	{
+		path: '/',
+		name: 'home',
+		component: () => import(/* webpackChunkName: "index-group" */ './views/Home.vue')
+	},
+	{
+		path: '/search/:typeName/:type',
+		name: 'search',
+		component: () => import(/* webpackChunkName: "index-group" */ './views/Search.vue'),
+		meta: {
+			title: '搜索'
+		}
+	},
+	{
+		path: '/article/:id',
+		name: 'article',
+		component: () => import(/* webpackChunkName: "index-group" */ './views/Article.vue')
+	},
+	{
+		path: '/log',
+		name: 'log',
+		component: () => import(/* webpackChunkName: "index-group" */ './views/Log.vue'),
+		meta: {
+			title: '归档'
+		}
+	},
+	{
+		path: '*',
+		name: '404',
+		component: () => import(/* webpackChunkName: "index-group" */ './views/404.vue'),
+		meta: {
+			title: '页面逃走了'
+		}
+	}
 ];
 
 const router = new VueRouter({
-    mode: 'history',
-    // mode: 'hash',
-    base: process.env.BASE_URL,
-    routes
+	mode: 'history',
+	// mode: 'hash',
+	base: process.env.BASE_URL,
+	routes
 });
 
 router.beforeEach((to, from, next) => {
-    if (to.meta.title) {
-        document.title = to.meta.title;
-    }
-    next();
+	if (to.meta.title) {
+		document.title = to.meta.title;
+	}
+	next();
 });
 
 export default router;
