963607ce8c7e73d1c2913422105099c6ff5fdbfb
zhangyifei233 <15204944127@163.com>
2020-04-20 01:07:36 +0800

    update 公共ciupload组件上传多文件

--- blog/src/components/common/ciupload.vue
+++ blog/src/components/common/ciupload.vue
@@ -6,6 +6,7 @@
             <input type="file"
                    class="ciupload-file"
                    name="ciupload"
+                   multiple="multiple"
                    ref="ciupload">
         </form>
         <button @click="click()">上传文件</button>
@@ -27,19 +28,22 @@ export default {
         getdom() {
             this.$nextTick(() => {
                 this.dom = this.$refs.ciupload;
-                this.dom.addEventListener("change", this.upload, false);
             });
         },
         click() {
+            this.dom.addEventListener("change", this.upload, false);
             this.dom.click();
         },
         upload() {
-            var fileObj = this.dom.files[0];
+            var fileObj = this.dom.files;
             var formData = new FormData();
-            formData.append("image", fileObj);
+            for (let i = 0; i < fileObj.length; i++) {
+                formData.append("image" + i, fileObj[i]);
+            }
             formData.append("aid", -2);
             this.$post("/upload/add", formData, data => {
                 this.$emit("afterUpload", data);
+                this.dom.removeEventListener("change", this.upload, false);
             });
         }
     }
@@ -49,5 +53,24 @@ export default {
 <style scoped>
 .ciupload-file {
     display: none;
+}
+button {
+    width: 100%;
+    display: inline-block;
+    line-height: 1;
+    white-space: nowrap;
+    cursor: pointer;
+    background-color: #409eff;
+    border: 1px solid #dcdfe6;
+    color: #fff;
+    text-align: center;
+    margin: 0;
+    font-weight: 500;
+    padding: 12px 20px;
+    font-size: 14px;
+    border-radius: 4px;
+}
+button:hover {
+    background-color: #40a0ffc7;
 }
 </style>
