75afe35e159196d8bb3f60803d1d984473768061
zhangyifei233 <15204944127@163.com>
2020-04-21 21:08:28 +0800

    refactor 配置文件

--- blog/vue.config.js
+++ blog/vue.config.js
@@ -50,13 +50,13 @@ module.exports = {
         hotOnly: false,
         port: '8020',
         proxy: {
-            '/api': {
+            '/api/ciblog': {
                 target: process.env.VUE_APP_URL, //API本地服务器的地址
                 // ws: true, //代理websockets
                 changeOrigin: true, // 虚拟的站点需要更管origin
                 pathRewrite: {
                     //重写路径 比如'/api/aaa/ccc'重写为'/aaa/ccc'
-                    '^/api': ''
+                    '^/api/ciblog': ''
                 }
             }
         },
