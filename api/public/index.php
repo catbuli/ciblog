93bfe80c1dd43c297033283c1b0057a2e3d58265
catbuli <15204944127@163.com>
2020-12-23 21:42:12 +0800

    update api

--- api/public/index.php
+++ api/public/index.php
@@ -2,27 +2,23 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-// 应用行为扩展定义文件
-return [
-    // 应用初始化
-    'app_init'     => [],
-    // 应用开始
-    'app_begin'    => [],
-    // 模块初始化
-    'module_init'  => [],
-    // 操作开始执行
-    'action_begin' => [],
-    // 视图内容过滤
-    'view_filter'  => [],
-    // 日志写入
-    'log_write'    => [],
-    // 应用结束
-    'app_end'      => [],
-];
+// [ 应用入口文件 ]
+namespace think;
+
+// 开启session
+session_start();
+
+// 加载基础文件
+require __DIR__ . '/../thinkphp/base.php';
+
+// 支持事先使用静态方法设置Request对象和Config对象
+
+// 执行应用并响应
+Container::get('app')->run()->send();
