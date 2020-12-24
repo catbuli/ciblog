8ac41a6084ac79b755296dbac95208b5016c138b
catbuli <15204944127@163.com>
2020-12-24 17:57:10 +0800

    add 数据库创建以及sql文件导入逻辑

--- script/init.js
+++ script/init.js
@@ -8,21 +8,36 @@
 
 "use strict"
 
-const process = require('process');
 
-
+var mysql = require('mysql');
 const { msgNormal, msgTitle, msgInfo, msgSuccess, msgError } = require("./consoleMsg");
+const exec = require('child_process').exec;
 const commandSpawn = require("./commandSpawn");
 const { resolvePath } = require('./utils');
 
 (async function init() {
     msgTitle("ciblog   项目初始化开始：");
 
-    // await commandSpawn('npm', ['install'], resolvePath("./", "./")).then(() => {
-    //     msgSuccess("脚本依赖安装成功");
-    // }).catch((err) => {
-    //     msgError(err);
-    // });
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123',
+    });
+
+    msgTitle("连接数据库：");
+    connection.connect();
+    msgSuccess("数据库连接成功");
+
+    msgTitle("创建数据库，导入初始数据：");
+    await connection.query("CREATE DATABASE if not exists ciblog", function (err, result) {
+        if (err) throw err;
+
+        var spl = `mysql -uroot -p123 ciblog < ${resolvePath("./", "database.sql")}`;
+        exec(spl, {}, (err) => {
+            if (err) throw err;
+        });
+    });
+    msgSuccess("数据库已创建，导入数据成功");
 
     msgTitle("前端项目依赖安装：");
     await commandSpawn('npm', ['install'], resolvePath("./", "blog")).then(() => {
@@ -37,4 +52,8 @@ const { resolvePath } = require('./utils');
     }).catch((err) => {
         msgError(err);
     });
+
+    connection.end();
+
+    msgTitle("请运行 npm run dev 启动开发环境");
 }())
--- script/run.js
+++ script/run.js
@@ -14,59 +14,12 @@ const process = require('process');
 
 const commandSpawn = require("./commandSpawn");
 const { resolvePath } = require('./utils');
+const { msgNormal, msgTitle, msgInfo, msgSuccess, msgError } = require("./consoleMsg");
 
 (async function run() {
 
-    commandSpawn('npm', ['run', `serve`], `${__dirname}/../blog`);
-
+    msgTitle("运行开发服务：");
     commandSpawn('php', ['think', `run`], `${__dirname}/../api`);
+    commandSpawn('npm', ['run', `serve`], `${__dirname}/../blog`);
 }())
 
-
-// return new Promise(function (resolve, reject) {
-
-//     var spawn_blog_ob = spawn_blog.spawn('npm', ['run', `serve`]);
-//     spawn_blog_ob.stdout.on('data', function (data) {
-//         console.log('stdout: ' + data);
-//     });
-
-//     spawn_blog_ob.stderr.on('data', function (data) {
-//         console.log('stderr: ' + data);
-//     });
-
-//     spawn_blog_ob.on('close', function (code) {
-//         console.log('子进程已退出，退出码 ' + code);
-//     });
-
-//     // // var cmd = "ipconfig";
-//     // // var blog = "cd /blog && npm run serve";
-//     // var blog = `cd ${__dirname}/blog && npm run serve`;
-//     // var api = `cd ${__dirname}/api && php think run`;
-//     // exec_blog(blog, {
-//     //     maxBuffer: 1024 * 2000
-//     // }, function (err, stdout, stderr) {
-//     //     console.log("blog", '')
-//     //     if (err) {
-//     //         console.log(err);
-//     //         reject(err);
-//     //     } else if (stderr.lenght > 0) {
-//     //         reject(new Error(stderr.toString()));
-//     //     } else {
-//     //         resolve();
-//     //     }
-//     // });
-//     // exec_api(api, {
-//     //     maxBuffer: 1024 * 2000
-//     // }, function (err, stdout, stderr) {
-//     //     console.log("api", '')
-//     //     if (err) {
-//     //         console.log(err);
-//     //         reject(err);
-//     //     } else if (stderr.lenght > 0) {
-//     //         reject(new Error(stderr.toString()));
-//     //     } else {
-//     //         console.log(stdout);
-//     //         resolve();
-//     //     }
-//     // });
-// });
